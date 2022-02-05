70e2308857591bb9b865701eac2fcace412c365a
kisa002 <vnycall74@naver.com>
2022-02-06 02:26:45 +0900

    [Add] Pretendard Fonts

--- app/src/main/java/com/haeyum/everyonewalking/ui/theme/Color.kt
+++ app/src/main/java/com/haeyum/everyonewalking/ui/theme/Color.kt
@@ -2,6 +2,10 @@ package com.haeyum.everyonewalking.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Main = Color(0xFF315CF3)
+val Sub = Color(0xFFFFB510)
+val Sub2 = Color(0xFF9D9D9D)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
--- app/src/main/java/com/haeyum/everyonewalking/ui/theme/Theme.kt
+++ app/src/main/java/com/haeyum/everyonewalking/ui/theme/Theme.kt
@@ -5,17 +5,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.haeyum.everyonewalking.R
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Main,
+    primaryVariant = Sub,
+    secondary = Sub2
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Main,
+    primaryVariant = Sub,
+    secondary = Sub2
 
     /* Other default colors to override
     background = Color.White,
@@ -25,6 +29,18 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
+)
+
+val pretendardFontFamily = FontFamily(
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+    Font(R.font.pretendard_extra_light, FontWeight.ExtraLight),
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.pretendard_black, FontWeight.Black),
 )
 
 @Composable
